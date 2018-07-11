c177696d6c0f88118c287a9a6c1e7829961cd550
ksnider92 <sni11001@byui.edu>
2018-07-10 19:59:12 -0600

    Added file reading/writing functions.

--- CR.cpp
+++ CR.cpp
@@ -4,8 +4,13 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <queue>
 #include <unistd.h>
 #include <RF24/RF24.h>
+
+#define read_File "toC.txt"
+#define write_File "toPy.txt"
+#define messageType unsigned long
 
 using namespace std;
 //RF24 radio("/dev/spidev0.0",8000000 , 25);  
@@ -18,6 +23,9 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 // hack to avoid SEG FAULT, issue #46 on RF24 github https://github.com/TMRh20/RF24.git
 unsigned long  got_message;
+
+bool writeToFile(string fileName, messageType message);
+queue<messageType> readFromFile(string);
 
 void setup(void){
 	//Prepare the radio module
@@ -42,6 +50,7 @@ int main( int argc, char ** argv){
 	setup();
 //	bool switched = false;
 	int counter = 0;
+	queue<messageType> messages;
 
 	//Define the options
 
@@ -71,30 +80,35 @@ int main( int argc, char ** argv){
         	//	}else{
                 		//If we received the message in time, let's read it and print it
                 		radio.read( &got_message, sizeof(unsigned long) );
-                		ofstream myfile;
+                		printf("Now writing %lu.\n\r",got_message);
+							writeToFile("t.txt", got_message);
+                		/* ofstream myfile;
                 		myfile.open ("t.txt");
                 		myfile << got_message;
-                		myfile.close();
-                		printf("Now writing %lu.\n\r",got_message);
+                		myfile.close(); */
                 //		return true;
 
         	}
 
 		else{
-			        unsigned long message = 2;
-			        radio.stopListening();
-        		//	unsigned long message = action;
-	        		printf("Now sending  %lu...", message);
+			        messages = readFromFile(read_File);
+						if (messages.size()) {
+								  unsigned long message = messages.front();;
+								  radio.stopListening();
+							//	unsigned long message = action;
+								printf("Now sending  %lu...", message);
 
-        				//Send the message
-        			bool ok = radio.write( &message, sizeof(unsigned long) );
-        			if (!ok){
-                			printf("failed...\n\r");
-        			}else{
-                			printf("ok!\n\r");
-        			}
-        			//Listen for ACK
-        			radio.startListening();
+									//Send the message
+								bool ok = radio.write( &message, sizeof(unsigned long) );
+								if (!ok){
+											printf("failed...\n\r");
+								}else{
+											printf("ok!\n\r");
+											messages.pop();
+								}
+								//Listen for ACK
+								radio.startListening();
+					  }
 			}
 
 //			printf("\n Talking with my NRF24l01+ friends out there....\n");
@@ -128,4 +142,77 @@ int main( int argc, char ** argv){
 
 	return 0;
 
+}
+
+/**********************************
+ * Write To File
+ **********************************
+ * Opens the given file, and adds
+ * the passed-in string into end
+ * of the file.
+ **********************************/
+bool writeToFile(string fileName, messageType data) {
+	/*if (testingFiles) {
+		printf("Writing to file.\n");
+	}*/
+	// Save all the data already in the file.
+	queue<messageType> oldData = readFromFile(fileName);
+
+	// Initialize the filestream.
+	ofstream file;
+	file.open(fileName.c_str());
+	
+	// Add the data to the back of the file.
+	oldData.push(data);
+
+	// Fill in the file.
+	while(!oldData.empty()) {
+		file << oldData.front();
+		oldData.pop();
+	}
+
+	// Wrap up the writing process.
+	file.close();
+	return true;
+}
+
+/**********************************
+ * Read From File
+ **********************************
+ * Opens the given file, returns a
+ * list of all the lines in the given
+ * file.
+ **********************************/
+queue<messageType> readFromFile(string fileName) {
+	//printf("Reading from file.\n");
+	
+	// Allocate local variables.
+	queue<messageType> *out = new queue<messageType>();
+	string ln;
+	ifstream in;
+	
+	// Access the file.
+	in.open(fileName.c_str(), ios_base::in);
+	while(!in.eof()) {
+		getline(in, ln);
+		if (ln != "") {
+			out->push(atoi(ln.c_str()));
+			printf("Read %s.\n", ln.c_str());
+		}
+	}
+	
+	// Close the file, and pass-back the lines.
+	in.close();
+	
+	// Initialize the filestream.
+	ofstream file;
+	file.open(fileName.c_str());
+
+	// Empty the file.
+	file << "";
+
+	// Wrap up the writing process.
+	file.close();
+	
+	return *out;
 }
